522568744e99754352e01f5654a1d4500f843152
arunneru <areinsdel@gmail.com>
2024-05-31 22:19:17 +0100

    plotted_track

--- extract_dat_from_matfiles.jl
+++ extract_dat_from_matfiles.jl
@@ -2,4 +2,4 @@ using MAT; using DelimitedFiles
 file = matopen("G:/Arun/neural_data/hc-6/Fiv/Fivpos09.mat")
 pos_dict = read(file, "pos")
 pos = pos_dict[9][4]["data"]
-writedlm("FivePos09_04.csv", pos, ",")
+writedlm("G:/Arun/neural_data/hc-6/Fiv/processed_data/FivePos09_04.csv", pos, ",")
